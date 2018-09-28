db917dfce3b72bb7a4ff39a77607f7394cb83003
Cristian Creteanu <cristiancreteanu06@gmail.com>
2018-09-28 11:50:24 +0300

    Moved change of the keepAliveTime of the thread pool executor

--- toughday/src/main/java/com/adobe/qe/toughday/internal/core/engine/runmodes/Normal.java
+++ toughday/src/main/java/com/adobe/qe/toughday/internal/core/engine/runmodes/Normal.java
@@ -233,7 +233,6 @@ public class Normal implements RunMode {
     }
 
     private void rampDown() {
-        ((ThreadPoolExecutor)testsExecutorService).setKeepAliveTime(1, TimeUnit.SECONDS);
         // every 'interval' milliseconds, we'll stop 'rate' workers
         if (end < start) {
             ThreadPoolExecutor executor = (ThreadPoolExecutor)testsExecutorService;
