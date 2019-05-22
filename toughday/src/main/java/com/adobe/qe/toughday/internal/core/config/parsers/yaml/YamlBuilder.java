meirosucristina/privateTD

Added the functionality to run TD distributed on Kubernetes.
Created a base docker image for toughday that will be used when running TD on Kubernetes.
Created a script that builds the above mentioned image and pushes this image in Azure(toughdaycr.azurecr.io container registry)

Added dockerfile

Used Java Spark framework to expose and http endpoint used for liveness probe when TD runs on Kubernetes.

[WIP] Trying to create the logic for diving the initial TD configuration into multiple
tasks to be distributed between the agents

Base logic for splitting a phase into multiple tasks to be distributed between the
agents.

Created two distinct docker images: one for the drivers and one for the agents. Driver exposes an endpoint
allowing new agents to register themselves.

A service must be exposed for the driver image in order to allow agents to easily communicate with this component inside the K8s cluster.

bug fix

bug fix

[WIP] - trigger driver from outside the cluster with HTTP query

Successfully sent the initial configuration to the driver

Implemented the basic logic for running TD distributed on K8s cluster.

Treated the corner case when the rate is smaller than the number of agents
in the cluster. For this particular case, a new parameter was introduced in the
run mode - initialDelay. Each agent will start with an initial delay equal to
agentId * interval milliseconds and the initial value of the interval property is
multiplied with the number of agents running in the cluster.

Exposed driver service to be accessible from outside the cluster
using nginx ingress controller and ingress resource.

Configured driver to send heartbeat messages to all the agents in the
cluster.

[WIP]
* Configured driver to periodically send heartbeat messages to the agents in order to
detect when a certain agent becomes unavailable and to ignore it when distributing the
task of the next phase.
* Changed YamlDumpConfiguration to fix small bug: timeout property(GlobalArgs) was modif

toughday/src/main/java/com/adobe/qe/toughday/internal/core/config/parsers/yaml/YamlBuilder.java
@@ -1,0 +1,95 @@
+package com.adobe.qe.toughday.internal.core.config.parsers.yaml;
+
+import com.adobe.qe.toughday.internal.core.ReflectionsContainer;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.introspector.Property;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
+
+import java.lang.reflect.Method;
+
+/**
+ * Class responsible for building and configuring a Yaml instance that knows how to dump a configuration
+ * in the desired format(without class tags, empty objects etc.)
+ */
+public class YamlBuilder {
+
+    private static Yaml instance = null;
+
+    // Configure yaml representer to exclude class tags when dumping an object.
+    private static void configureYamlRepresenterToExcludeClassTags(Representer representer) {
+        // Tag.MAP is by default ignored when dumping an object
+        representer.addClassTag(GenerateYamlConfiguration.class, Tag.MAP);
+        representer.addClassTag(YamlDumpConfiguration.class, Tag.MAP);
+        for (Class type : ReflectionsContainer.getSubTypesOf(YamlDumpAction.class)) {
+            representer.addClassTag(type, Tag.MAP);
+        }
+
+    }
+
+    private static void buildInstance() {
+        DumperOptions dumperOptions = getDumperOptions();
+
+        // Configure the representer to ignore empty fields when dumping the object. By default, each empty filed is represented as {}.
+        Representer representer = getRepresenter();
+        configureYamlRepresenterToExcludeClassTags(representer);
+
+        Constructor constructor = new Constructor(GenerateYamlConfiguration.class);
+        instance = new Yaml(constructor, representer, dumperOptions);
+    }
+
+    /**
+     * Getter for the Yaml instance.
+     */
+    public static Yaml getYamlInstance() {
+        if (instance == null) {
+            buildInstance();
+        }
+
+        return instance;
+    }
+
+
+    private static Representer getRepresenter() {
+        return new Representer() {
+            @Override
+            protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue, Tag customTag) {
+
+                Method method = null;
+                try {
+                    if (propertyValue == null) {
+                        propertyValue = "";
+                    }
+                    method = propertyValue.getClass().getMethod("isEmpty");
+                } catch (NoSuchMethodException ignored) { }
+
+                if (method == null) {
+                    return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+                } else {
+
+                    try {
+                        if (Boolean.valueOf(method.invoke(propertyValue).toString())) {
+                            return null;
+                        }
+                    } catch (Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                    return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+                }
+            }
+        };
+    }
+
+    private static DumperOptions getDumperOptions() {
+        DumperOptions dumperOptions = new DumperOptions();
+
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        dumperOptions.setAllowReadOnlyProperties(true);
+
+        return dumperOptions;
+    }
+}

adobe/toughday2

toughday/src/main/java/com/adobe/qe/toughday/internal/core/config/parsers/yaml/YamlBuilder.java
@@ -1,0 +1,98 @@
+package com.adobe.qe.toughday.internal.core.config.parsers.yaml;
+
+import com.adobe.qe.toughday.internal.core.ReflectionsContainer;
+import com.adobe.qe.toughday.internal.core.distributedtd.YamlDumpConfigurationAsTaskForTDAgents;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.introspector.Property;
+import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
+
+import java.lang.reflect.Method;
+
+/**
+ * Class responsible for building and configuring a Yaml instance that knows how to dump a configuration
+ * in the desired format(without class tags, empty objects etc.)
+ */
+public class YamlBuilder {
+
+    private static Yaml instance = null;
+
+    // Configure yaml representer to exclude class tags when dumping an object.
+    private static void configureYamlRepresenterToExcludeClassTags(Representer representer) {
+        // Tag.MAP is by default ignored when dumping an object
+        representer.addClassTag(GenerateYamlConfiguration.class, Tag.MAP);
+        representer.addClassTag(YamlDumpConfigurationAsTaskForTDAgents.class, Tag.MAP);
+        for (Class type : ReflectionsContainer.getSubTypesOf(YamlDumpAction.class)) {
+            representer.addClassTag(type, Tag.MAP);
+        }
+
+    }
+
+    private static void buildInstance() {
+        DumperOptions dumperOptions = getDumperOptions();
+
+        // Configure the representer to ignore empty fields when dumping the object. By default, each empty filed is represented as {}.
+        Representer representer = getRepresenter();
+        configureYamlRepresenterToExcludeClassTags(representer);
+
+        Constructor constructor = new Constructor(GenerateYamlConfiguration.class);
+        instance = new Yaml(constructor, representer, dumperOptions);
+    }
+
+    /**
+     * Getter for the Yaml instance.
+     */
+    public static Yaml getYamlInstance() {
+        if (instance == null) {
+            buildInstance();
+        }
+
+        return instance;
+    }
+
+
+    private static Representer getRepresenter() {
+        return new Representer() {
+            @Override
+            protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue, Tag customTag) {
+
+                Method method = null;
+                try {
+                    if (propertyValue == null) {
+                        propertyValue = "";
+                    }
+                    method = propertyValue.getClass().getMethod("isEmpty");
+                } catch (NoSuchMethodException ignored) { }
+
+                if (method == null) {
+                    return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+                } else {
+
+                    try {
+                        if (Boolean.valueOf(method.invoke(propertyValue).toString())) {
+                            return null;
+                        }
+                    } catch (Throwable t) {
+                        /* no action required here. The only implication is that the empty collection will be included
+                         * in the Yaml representation when dumping an object.
+                         */
+                    }
+
+                    return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+                }
+            }
+        };
+    }
+
+    private static DumperOptions getDumperOptions() {
+        DumperOptions dumperOptions = new DumperOptions();
+
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        dumperOptions.setAllowReadOnlyProperties(true);
+
+        return dumperOptions;
+    }
+}